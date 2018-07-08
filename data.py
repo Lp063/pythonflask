e436c2724b44f57dd252c5a9751e49633caf67fe
Lohit <lohit.pereira09@stanislausbandra.in>
2018-07-08 22:31:54 +0530

    issue with installing flask_mysqldb

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title':'This is a title One',
+            'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quod autem sapiente cupiditate libero, expedita in culpa officiis quis amet optio repellat ad possimus, deleniti numquam. Totam debitis odit harum?',
+            'author':'Lohit Pereira',
+            'createDate':'09-07-2018'
+        },
+        {
+            'id':2,
+            'title':'This is a title Two',
+            'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quod autem sapiente cupiditate libero, expedita in culpa officiis quis amet optio repellat ad possimus, deleniti numquam. Totam debitis odit harum?',
+            'author':'Lohit Pereira',
+            'createDate':'09-07-2018'
+        },
+        {
+            'id':3,
+            'title':'This is a title Three',
+            'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quod autem sapiente cupiditate libero, expedita in culpa officiis quis amet optio repellat ad possimus, deleniti numquam. Totam debitis odit harum?',
+            'author':'Lohit Pereira',
+            'createDate':'09-07-2018'
+        }
+    ]
+
+    return articles
